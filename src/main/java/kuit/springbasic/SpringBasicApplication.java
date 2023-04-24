8babf6f5817aad5ee5e06ab0d7a4aaf01924ceb4
euna <christinejung10@daum.net>
2023-04-24 21:45:57 +0900

    fix(SpringBasicApplication): @ServletComponentScan 애노테이션 삭제

--- src/main/java/kuit/springbasic/SpringBasicApplication.java
+++ src/main/java/kuit/springbasic/SpringBasicApplication.java
@@ -2,14 +2,12 @@ package kuit.springbasic;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.servlet.ServletComponentScan;
 
-@ServletComponentScan
 @SpringBootApplication
 public class SpringBasicApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringBasicApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBasicApplication.class, args);
+    }
 
 }
